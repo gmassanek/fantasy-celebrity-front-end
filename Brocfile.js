582c865fb8fddd8c6391233fe9d1111587b617d6
Geoff Massanek <gmassanek@gmail.com>
2015-06-16 10:57:30 -0400

    Added theme index, with all dependencies (but avatars) present

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,27 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import("bower_components/fontawesome/css/font-awesome.min.css");
+app.import("bower_components/fontawesome/fonts/FontAwesome.otf", { destDir: 'fonts' });
+app.import("bower_components/fontawesome/fonts/fontawesome-webfont.eot", { destDir: 'fonts' });
+app.import("bower_components/fontawesome/fonts/fontawesome-webfont.svg", { destDir: 'fonts' });
+app.import("bower_components/fontawesome/fonts/fontawesome-webfont.ttf", { destDir: 'fonts' });
+app.import("bower_components/fontawesome/fonts/fontawesome-webfont.woff", { destDir: 'fonts' });
+app.import("bower_components/fontawesome/fonts/fontawesome-webfont.woff2", { destDir: 'fonts' });
+app.import("bower_components/slimscroll/jquery.slimscroll.min.js");
+app.import("bower_components/flot/excanvas.min.js");
+app.import("bower_components/flot/jquery.flot.js");
+app.import("bower_components/flot/jquery.flot.pie.js");
+app.import("bower_components/flot/jquery.flot.resize.js");
+app.import("bower_components/flot/jquery.flot.time.js");
+app.import("bower_components/flot.tooltip/js/jquery.flot.tooltip.js");
+
+app.import("bower_components/bootstrap/dist/css/bootstrap.min.css");
+app.import("bower_components/bootstrap/dist/js/bootstrap.min.js");
+
+app.import("vendor/mvpready-admin.css");
+app.import("vendor/mvpready-core.js");
+app.import("vendor/mvpready-helpers.js");
+app.import("vendor/mvpready-admin.js");
+
 module.exports = app.toTree();
