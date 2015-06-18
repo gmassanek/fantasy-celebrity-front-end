c753d20d44b57a5ea9a7359abc9d6183b418b022
Geoff Massanek <gmassanek@gmail.com>
2015-06-18 15:41:00 -0400

    Team show template

--- app/leagues/teams/show/route.js
+++ app/leagues/teams/show/route.js
@@ -2,6 +2,129 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return params;
+    return {
+      id: params.id,
+      roster: [
+        {
+          position: 'Reality Star',
+          name: 'Paula Abdul',
+          points: {
+            legal: 0,
+            fighting: 0,
+            career: 0,
+            marital: 0,
+            health: 0,
+            other: 0,
+            total: 0
+          }
+        }, {
+          position: 'Actor',
+          name: 'Jack Nicholson',
+          points: {
+            legal: 0,
+            fighting: 0,
+            career: 0,
+            marital: 0,
+            health: 0,
+            other: 0,
+            total: 0
+          }
+        }, {
+          position: 'Actor',
+          name: 'Mel Gibson',
+          points: {
+            legal: 0,
+            fighting: 0,
+            career: 0,
+            marital: 0,
+            health: 0,
+            other: 0,
+            total: 0
+          }
+        }, {
+          position: 'Musician',
+          name: 'Whitney Houston',
+          points: {
+            legal: 0,
+            fighting: 0,
+            career: 0,
+            marital: 0,
+            health: 0,
+            other: 0,
+            total: 0
+          }
+        }, {
+          position: 'Athlete',
+          name: 'Chad Ocho Cinco',
+          points: {
+            legal: 15,
+            fighting: 0,
+            career: 0,
+            marital: 0,
+            health: 0,
+            other: 0,
+            total: 15
+          }
+        }, {
+          position: 'Other',
+          name: 'Pants on the Ground Guy',
+          points: {
+            legal: 0,
+            fighting: 0,
+            career: 0,
+            marital: 0,
+            health: 0,
+            other: 0,
+            total: 0
+          }
+        }, {
+          position: 'Politician',
+          name: 'Mitt Romney',
+          points: {
+            legal: 0,
+            fighting: 0,
+            career: 0,
+            marital: 0,
+            health: 0,
+            other: 0,
+            total: 0
+          }
+        }, {
+          position: 'Other',
+          name: 'Gretchen Rossi',
+          points: {
+            legal: 0,
+            fighting: 0,
+            career: 0,
+            marital: 0,
+            health: 0,
+            other: 0,
+            total: 0
+          }
+        }, {
+          position: 'Dead People',
+          name: 'King Tut',
+          points: {
+            legal: 0,
+            fighting: 0,
+            career: 0,
+            marital: 0,
+            health: 0,
+            other: 0,
+            total: 0
+          }
+        }
+      ],
+      pointSubmissions: [
+        {
+          date: "2015-01-01",
+          player: "Chad Ocho Cinco",
+          category: "Legal",
+          action: "Domestic Abuse",
+          points: 15,
+          status: "approved"
+        }
+      ]
+    };
   }
 });
