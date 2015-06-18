1da219d114c86eefe7aa9c66fa8882292ed40b6b
Geoff Massanek <gmassanek@gmail.com>
2015-06-18 15:51:34 -0400

    Move data loading for league positions to the route

--- app/leagues/positions/route.js
+++ app/leagues/positions/route.js
@@ -1,4 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return {
+      positions: [ {
+        name: "Actors",
+        slots: 2
+      }, {
+        name: "Athletes",
+        slots: 1
+      }, {
+        name: "Politicians",
+        slots: 1
+      }, {
+        name: "Miscellaneous",
+        slots: 2
+      }, {
+        name: "Reality Starts",
+        slots: 1
+      }, {
+        name: "Musicians",
+        slots: 1
+      }, {
+        name: "Dead People",
+        slots: 1
+      } ]
+    };
+  }
 });
