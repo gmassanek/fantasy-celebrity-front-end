6b2d83b28c04af46c42674eaca874baf058a68fe
Geoff Massanek <gmassanek@gmail.com>
2015-06-27 10:17:18 -0400

    Move scoring to the route

--- app/leagues/scoring/route.js
+++ app/leagues/scoring/route.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return {
+      pointCategories: [
+        { category: "Legal", name: "DUI", points: 20 },
+        { category: "Legal", name: "Drug Posession", points: 20 },
+        { category: "Legal", name: "Disorderly Conduct", points: 45 },
+        { category: "Career", name: "Leaked Sex Tape", points: 30 },
+        { category: "Career", name: "Leaked Nude Pics", points: 25 }
+      ]
+    }
+  }
 });
