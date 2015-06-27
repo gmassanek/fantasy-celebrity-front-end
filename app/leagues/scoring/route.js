92a3bf43f36160b50d5e8c1ec590fd5bc289a318
Geoff Massanek <gmassanek@gmail.com>
2015-06-27 10:37:38 -0400

    Introduce an apiRoot and do a real ajax call

--- app/leagues/scoring/route.js
+++ app/leagues/scoring/route.js
@@ -1,15 +1,9 @@
 import Ember from 'ember';
+import ajax from 'ic-ajax';
 
 export default Ember.Route.extend({
   model() {
-    return {
-      pointCategories: [
-        { category: "Legal", name: "DUI", points: 20 },
-        { category: "Legal", name: "Drug Posession", points: 20 },
-        { category: "Legal", name: "Disorderly Conduct", points: 45 },
-        { category: "Career", name: "Leaked Sex Tape", points: 30 },
-        { category: "Career", name: "Leaked Nude Pics", points: 25 }
-      ]
-    }
+    var uri = `${EmberENV.apiRoot}/api/v1/status`;
+    return ajax(uri);
   }
 });
