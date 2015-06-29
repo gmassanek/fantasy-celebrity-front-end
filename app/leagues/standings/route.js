ed3270ba80a8dc40815b819d7e3857b1136750f9
Geoff Massanek <gmassanek@gmail.com>
2015-06-29 16:42:49 -0500

    Get teams from api

--- app/leagues/standings/route.js
+++ app/leagues/standings/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.find('team', { league_id: 1 }).then(function(data) {
+      return data;
+    }).fail(function(e) {
+      console.log(e);
+    });
+  }
 });
